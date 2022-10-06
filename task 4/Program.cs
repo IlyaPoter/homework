4d78579576b9e93bb59468752c1bdeb88c9ae442
study <study@lan.sibadi.org>
2022-10-07 00:01:30 +0600

    task 4

---
+++ task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+while (count<=number)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count=count+1;
+
+}
