7bced52f8416bc059ee7fa9173589cd65f5bb717
study <study@lan.sibadi.org>
2022-10-06 23:37:26 +0600

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("2 Hello, World");
+﻿
+Console.WriteLine("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+if (numberOne>numberTwo)
+{
+    Console.WriteLine("Максимальное число первое: "+numberOne );
+}
+else
+{
+    Console.WriteLine("Максимальное число второе: "+numberTwo );
+}
+if (numberOne==numberTwo)
+{
+    Console.WriteLine("Числа равны: "+numberOne+" = "+numberTwo);
+}
