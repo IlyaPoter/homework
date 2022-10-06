9edf1aa21a0f3f26481140bac01dff73e83076bf
study <study@lan.sibadi.org>
2022-10-06 23:45:32 +0600

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberThree = int.Parse(Console.ReadLine());
+int max = numberOne;
+if (numberTwo>max)
+{
+    max=numberTwo;
+}
+if (numberThree>max)
+{
+    max=numberThree;
+}
+Console.WriteLine("Максимальное число: "+max);
