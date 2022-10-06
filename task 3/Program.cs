14233c9c2e6200d778606eca4bf84f198347ec43
study <study@lan.sibadi.org>
2022-10-06 23:52:31 +0600

    task 3

---
+++ task 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine("Число делится без остатка на 2");
+}
+else
+{
+    Console.WriteLine("Число не делится без остатка на 2");
+}
