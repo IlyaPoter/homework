64afa26b4a41ba9b97e63d267277984de713ddeb
study <study@lan.sibadi.org>
2022-10-09 15:23:45 +0600

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру дня недели: ");
+int numberWeek = int.Parse(Console.ReadLine());
+if (numberWeek < 1 | numberWeek > 7)
+{
+    Console.WriteLine("Такого дня недели не существует. Введите число в диапазоне от 1 до 7");
+}
+else if (numberWeek == 6 | numberWeek == 7)
+{
+    Console.WriteLine("Указанный день - выходной");
+}
+else
+{
+    Console.WriteLine("Указанный день не является выходным");
+}
+
+if (numberWeek==null)
+{
+    Console.WriteLine("Пустой ввод");
+}
