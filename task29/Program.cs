81a34d505c6628bc709d47dad4c2081d4f717d20
study <study@lan.sibadi.org>
2022-10-26 22:35:30 +0600

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(0,100);
+        Console.Write($"{res[i]} ");
+
+    }
+    return res;
+}
+
+int SumNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i=0;i<array.Length;i=i+2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = GetArray(5);
+int result = SumNumbers(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {result}");
