4d7d9d60f8d698f3f12652e0a428da4c5fbcbd4a
study <study@lan.sibadi.org>
2022-10-30 21:10:47 +0600

    initial commit

---
+++ task31/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Задача 41. Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь
+
+void Main()
+{
+    Console.Clear();
+    int[] array = GetNumber();
+    int result = TestNumber(array);
+    Console.WriteLine($"Количество введенных чисел больше нуля: {result}");
+}
+
+int[] GetNumber()
+{
+    Console.Write("Введите числа через пробел: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return arr;
+}
+
+int TestNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Main();
